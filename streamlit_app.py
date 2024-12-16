3b4519ca2109257dc036196b7830413d9aa55d3c
sarthakacc360 <sbansal@accelerize360.com>
2024-12-16 20:22:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,10 @@ session = cnx.session()
 name_on_order = st.text_input('Name on smoothie')
 st.write('The name on your smoothie will be ' , name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME') , col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+                                                                      
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients',
     my_dataframe,
