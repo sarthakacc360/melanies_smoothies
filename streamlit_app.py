fa5228e7d6d2c2cebfae8a1098b8e9b2bde975e0
sarthakacc360 <sbansal@accelerize360.com>
2024-12-16 21:24:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,13 @@ name_on_order = st.text_input('Name on smoothie')
 st.write('The name on your smoothie will be ' , name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME') , col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
-                                                                      
+
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients',
     my_dataframe,
