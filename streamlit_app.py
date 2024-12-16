25af9a5d30d6ada5bdaea18191465fd81cadda73
sarthakacc360 <sbansal@accelerize360.com>
2024-12-16 19:33:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,6 +11,9 @@ st.write(
     [docs.streamlit.io](https://docs.streamlit.io).
     """
 )
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 name_on_order = st.text_input('Name on smoothie')
 st.write('The name on your smoothie will be ' , name_on_order)
